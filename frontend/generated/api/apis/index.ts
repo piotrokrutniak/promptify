ecdf56fb0730fb139f9adf6183f245503fbff354
Piotr Okrutniak <piotrokrutniak@gmail.com>
2026-06-10 15:10:48 +0200

    feat(api): generate initial API client files and models using OpenAPI Generator

---
+++ frontend/generated/api/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './PromptsApi';
+export * from './SessionsApi';
+export * from './UsersApi';
---
+++ frontend/generated/api/models/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccessTokenResponse';
+export * from './AccessTokenResponseExpiresIn';
+export * from './CancelPromptPromptIdParameter';
+export * from './CreatePromptRequest';
+export * from './CreateSessionRequest';
+export * from './CreateSessionResponse';
+export * from './ForgotPasswordRequest';
+export * from './HttpValidationProblemDetails';
+export * from './HttpValidationProblemDetailsStatus';
+export * from './InfoRequest';
+export * from './InfoResponse';
+export * from './LoginRequest';
+export * from './PromptDto';
+export * from './RefreshRequest';
+export * from './RegisterRequest';
+export * from './ResendConfirmationEmailRequest';
+export * from './ResetPasswordRequest';
+export * from './SessionDto';
+export * from './SessionListItemDto';
+export * from './TwoFactorRequest';
+export * from './TwoFactorResponse';
